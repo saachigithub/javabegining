fc0a10aebcced59ec5293f613445cede51bc4bf0
Saachi Gupta <92663616+saachigithub@users.noreply.github.com>
2023-01-05 01:34:17 +0530

    Add files via upload
    Basic Program but also have documentation which is in html form

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * Programmer:Saachi
+ * Program: Hello World
+ * This program is to show how documentation is done
+ * This is my frist documention and it is in html form.
+ * Whatever is 'public' will be shown in html file name'index'.
+ **/
+public class HelloWorld {
+void hello() {
+System.out.println("Hello World!");
+}
+public static void main(String args[]) {
+hello();
+}
+}
